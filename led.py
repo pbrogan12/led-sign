1fe971ee882b00c172786f525555eb168c177d17
pbrogan <pbrogan12@gmail.com>
2012-11-20 22:04:14 -0500

    Now integrates tweets

--- led.py
+++ led.py
@@ -5,28 +5,17 @@
 
 # move a beach ball image in a pygame window with wrapping
 
-import pygame, sys, feedparser
-d = feedparser.parse('http://www.sportsnetwork.com/aspdata/clients/sportsnetwork/NFLrssscores.aspx')
+import pygame, sys, feedparser, twitter
 c = 0
-t = 0
-msg = ""
-msg1 = ""
-for i in d.entries:
-    a = i.title
-    if c / 2 == 1:
-        msg = msg + ' ' + a
-        c += 1
-    else:
-        msg1 = msg1 + ' ' + a
-        c += 1
+tweets = twitter.get_tweets()
 pygame.init()
 pygame.mouse.set_visible(0)
 screen = pygame.display.set_mode([0,0] ,pygame.FULLSCREEN)
 screen.fill([0, 0, 0])
-font = pygame.font.SysFont('astronautiii',75)
-text = font.render(msg + msg1, False, (255,0,0))
-fontSize = font.size(msg + msg1)
-x = screen.get_width() + 40
+font = pygame.font.SysFont('astronaut',200)
+text = font.render(tweets[c] , False, (255,0,0))
+fontSize = font.size(tweets[c])
+x = screen.get_width()
 y = screen.get_height() / 2
 x_speed = -10
 
@@ -40,21 +29,14 @@ while 1:
     pygame.time.delay(30)
     pygame.draw.rect(screen, [0,0,0], [x, y, fontSize[0], fontSize[1]], 0)
     x = x + x_speed
-    if x < -int(fontSize[0]) + -60:
-        t += 1
-        if t >= 2:
-            msg = "FOOOOOOOOOOOOOOOOOOBAT"
-            msg1 = "THISISTHEBOMB"
-            font = pygame.font.SysFont('astronaut',100)
-            text = font.render(msg + msg1, False, (255,0,0))
-            fontSize = font.size(msg + msg1)
-            x = 1080
-            screen.blit(text, [x, y])
-            pygame.display.update()
-        else:
-            x = 1080
-            screen.blit(text, [x, y])
-            pygame.display.update()
+    if x < -int(fontSize[0]):
+        c += 1
+        font = pygame.font.SysFont('astronaut',200)
+        text = font.render(tweets[c], False, (255,0,0))
+        fontSize = font.size(tweets[c])
+        x = screen.get_width() + 100
+        screen.blit(text, [x, y])
+        pygame.display.update()
     else:
         screen.blit(text, [x, y])
         pygame.display.update()
