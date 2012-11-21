6f65f49dd2f5090287358a2ea52f1accc374393b
pbrogan <pbrogan12@gmail.com>
2012-11-20 21:43:15 -0500

    Added twitter function to get tweets for led sign.

---
+++ twitter.py
@@ -0,0 +1,8 @@
+import feedparser
+def get_tweets():
+    d = feedparser.parse('http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=jimcramer')
+    tweet_list = []
+    for i in d.entries[:5]:
+        tweet_list.append(i.title)
+    print tweet_list
+get_tweets()
